c3721ed23996e7145510f8e83e5b73408fb8b8c9
Joseph Cryer <jcryer1234@gmail.com>
2021-02-13 13:48:06 +0000

    Extra Code

---
+++ ExtraCode/public/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#example1').click(function() {
+        window.location = "example1"
+    });
+
+    $('#example2').click(function() {
+        window.location = "example2"
+    });
+});
