eaabbfb57a049b2062982f375ac2b871c82b8f26
Alfie Richards <alfie@backslash.build>
2021-10-04 12:24:55 +0100

    move to root dir of repo

--- ExtraCode/public/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#example1').click(function() {
-        window.location = "example1"
-    });
-
-    $('#example2').click(function() {
-        window.location = "example2"
-    });
-});
